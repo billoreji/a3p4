45753c295b1503ea43220ee2a8432ad57b5985da
sunnykurmi <shivanibillore22@gmail.com>
2024-05-25 15:04:55 +0530

    my commit

---
+++ src/pages/Fees.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Fees() {
+  return (
+    <div>
+        <h1>Fees</h1>
+    </div>
+  )
+}
+
+export default Fees
---
+++ src/pages/Learn.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Learn() {
+  return (
+    <div><h1>Learn</h1></div>
+  )
+}
+
+export default Learn
